b4cb44c7c8ca85fa7358d91382ccb5e57c716c47
Joakim Persson <Joakiimm@hotmail.com>
2018-09-28 11:44:14 +0200

    Added Machine Menu

---
+++ T-800/Domain/MachineMenu.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace T_800.Domain
+{
+    class MachineMenu : Machine
+    {
+        public MachineMenu(string name) : base(name)
+        {
+
+        }
+
+        public void RobotMenu()
+        {
+            bool menu = true;
+            while (menu)
+            {
+                Console.WriteLine("Welcome to robot" + " " + "**Lägg till namn här**");//Lägg till namn från Machine listan
+                Console.WriteLine("\n[1]");
+                Console.WriteLine("[2]");
+                Console.WriteLine("[3]Exit Machine");
+
+                string menuChoice = Console.ReadLine();
+
+                switch (menuChoice)
+                {
+                    case "1":
+                        {
+                            break;
+                        }
+                    case "2":
+                        {
+                            break;
+                        }
+                    case "3":
+                        {
+                            Console.WriteLine("Do you want to return to Main menu? Y/N: ");
+                            string exit = Console.ReadLine();
+                            if (exit == "Y" || exit == "y")
+                            {
+                                Console.WriteLine("Goodbye");
+                                break;
+                            }
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
