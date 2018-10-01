df24c7ecfc4d465d740077b7e09e065960d79369
Joakim Persson <Joakiimm@hotmail.com>
2018-10-01 10:50:13 +0200

    Changes in MachineMenu

--- T-800/Domain/Menu.cs
+++ T-800/Domain/Menu.cs
@@ -14,9 +14,9 @@ namespace T_800
                 Console.Clear();
                 Console.WriteLine("\t=====Welcome to Slynet 1.7=====");
                 Console.WriteLine("\n\tPlease choose your option below.");
-                Console.WriteLine("\t[1]");
-                Console.WriteLine("\t[2]");
-                Console.WriteLine("\t[3]");
+                Console.WriteLine("\t[1]Select Terminator");
+                Console.WriteLine("\t[2]Selfdestruct Terminators");
+                Console.WriteLine("\t[3]Set Mission");
                 Console.WriteLine("\t[4]Quit");
 
                 string menuChoice = Console.ReadLine();
@@ -24,34 +24,37 @@ namespace T_800
                 switch (menuChoice)
                 {
                     case "1":
-                    {
-                        Console.Clear();
-                        break;
-                    }
+                        {
+                            Console.WriteLine("\tPlease select Terminator");
+                            //Lista maskiner här
+                            Console.Clear();
+                            break;
+                        }
                     case "2":
-                    {
-                        Console.Clear();
-                        break;
-                    }
+                        {
+                            Console.Clear();
+                            break;
+                        }
                     case "3":
-                    {
-                        Console.Clear();
-                        break;
-                    }
+                        {
+                           
+                            Console.Clear();
+                            break;
+                        }
                     case "4":
-                    {
-                        Console.Clear();
-                        Console.Write("\tDo you want to quit? Y/N: ");
-                        string quit = Console.ReadLine();
-                        if (quit == "Y" || quit == "y")
                         {
-                            menu = false;
+                            Console.Clear();
+                            Console.Write("\tDo you want to quit? Y/N: ");
+                            string quit = Console.ReadLine();
+                            if (quit == "Y" || quit == "y")
+                            {
+                                menu = false;
+                            }
+                            else
+                            {
+                            }
+                            break;
                         }
-                        else
-                        {
-                        }
-                        break;
-                    }
                     default:
                         Console.WriteLine("\tChoose a number between 1-4");
                         break;
